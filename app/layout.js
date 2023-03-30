357b9acaf9ffd4e00076870109768a740140ca03
hasnainirfan <hasnainirfanforlaptop@gmail.com>
2023-03-30 16:47:42 +0500

    check

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <body>{children}</body>
     </html>
   )
